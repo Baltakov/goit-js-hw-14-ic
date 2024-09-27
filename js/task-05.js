cb3a6772cdc83206eee27ba266ab51f5a81be308
Kostiantyn Baltakov <tira2500@gmail.com>
2024-09-27 19:42:16 +0300

    add slider

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,10 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
-console.log(input);
 
 input.addEventListener("input", () => {
   if (input.value === "") {
     output.textContent = "Anonymous";
   } else {
     output.textContent = input.value;
-    console.log(input.value);
   }
 });
--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,11 @@ const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", () => {
   const inputValue = input.value;
-  console.log(inputValue);
   if (input.value.length !== parseInt(input.dataset.length)) {
     input.classList.add("invalid");
     input.classList.remove("valid");
-    console.log(1);
   } else {
     input.classList.add("valid");
     input.classList.remove("invalid");
-    console.log(2);
   }
 });
