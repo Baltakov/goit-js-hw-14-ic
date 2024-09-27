b20edfafb5a99e29e090d5d3855c459320064ffe
Kostiantyn Baltakov <tira2500@gmail.com>
2024-09-27 20:52:08 +0300

    add submit form

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const submitForm = { email, password };
+  console.log(submitForm);
+  console.log(`Email: ${form.elements[0].value}`);
+  console.log(`Password: ${form.elements[1].value}`);
+  form.reset();
+});
